cfc949867aa60a8c88c40c128b153c00cb6bb97f
klaus <405471103@qq.com>
2017-11-08 15:25:19 +0800

    日常维护

--- company_item_3/ajax/massMP4.js
+++ company_item_3/ajax/massMP4.js
@@ -13,13 +13,11 @@ var transliteration = require("transliteration");
 var sign = require("../func/sign.js");
 var fs = require("fs");
 var Fiber = require('fibers');
+var alioss = require('../func/alioss.js');
 
-console.log()
 
 config.readfile();
-//var alioss = require('../func/alioss.js');
 var co = require('co');
-var OSS = require('ali-oss');
 
 
 module.exports.run = function(body, pg, mo) {
@@ -27,40 +25,36 @@ module.exports.run = function(body, pg, mo) {
     var f = {};
     body.receive = JSON.parse(body.data);
     f.data = body.receive;
-    f.session = body.session;
+    //f.session = body.session;
 
     f.时间 = moment().format('YYYYMMDDHHmmss');
-
+    var conf = config.get("alicloud").阿里云参数1;
+    var client = alioss.init(conf.region, conf.accessKeyId, conf.accessKeySecret);
     // 生成随机数
-    var chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    
+    var chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
     function generateMixed(n) {
-         var res = "";
-         for(var i = 0; i < n ; i ++) {
-             var id = Math.ceil(Math.random()*35);
-             res += chars[id];
-         }
-         return res;
+        var res = "";
+        for (var i = 0; i < n; i++) {
+            var id = Math.ceil(Math.random() * 35);
+            res += chars[id];
+        }
+        return res;
     }
-    
+
     f.fileName = f.时间 + generateMixed(4);
-    var client = new OSS({
-        region: 'oss-cn-shenzhen',
-        accessKeyId: 'LTAIRz4pA6Qeu12D',
-        accessKeySecret: 'ZASbh3Xg1RtSo6VxwLnNkSlNvXNMYJ',
-    });
     var fiber = Fiber.current;
     var data = {};
-    var timeArr=[];
+    var timeArr = [];
     co(function*() {
         client.useBucket('zyk-temp');
-		
-        data.result = yield client.multipartUpload(f.fileName, f.data.img_list,{
-        	progress: function*(p) {
+
+        data.result = yield client.multipartUpload(f.fileName, f.data.img_list, {
+            progress: function*(p) {
                 console.log('上传中: ' + (p * 100).toFixed(2) + '%');
                 timeArr = (p * 100).toFixed(2);
                 // console.log(timeArr)
-                body.session.progress = timeArr;                                      
+                // body.session.progress = timeArr;
 
             }
         });
@@ -78,15 +72,12 @@ module.exports.run = function(body, pg, mo) {
         f.状态 = '上传失败';
     }
 
+
+
     p.状态 = f.状态;
     p.name = 信息.name;
-    p.上传时间=timeArr;
+    p.上传时间 = timeArr;
     p.地址 = (信息.res.requestUrls)[0].split("?")[0];
-
-    console.log(p)
-
-
-
     return common.removenull(p);
 
 }
