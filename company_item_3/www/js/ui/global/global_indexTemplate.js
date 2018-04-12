5e965d8c06208064dd07f90495956eadfed5a9e4
KlausChan0523 <xing930629@163.com>
2018-04-12 18:04:40 +0800

    整理代码

--- company_item_3/www/js/ui/global/global_indexTemplate.js
+++
@@ -1,5 +0,0 @@
-
-var form_special_control={};
-form_special_control.id = 1;
-form_special_control.state = ["显示","不显示"];
-form_special_control.pic = ["图片地址"];
--- company_item_3/www/page/test/b.js
+++
@@ -1,2 +0,0 @@
-import {sum} from './a.js'
-console.log(sum(1,2))
